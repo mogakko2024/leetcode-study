8e8f357d97d6f35184dfb67e9929b42c6106f98c
tom-chulhyun <tom.chulhyun@gmail.com>
2026-01-10 00:19:07 +0900

    Update 철현_MaximizeHappinessOfSelectedChildren.py

--- 1주차/철현_MaximizeHappinessOfSelectedChildren.py
+++ 1주차/철현_MaximizeHappinessOfSelectedChildren.py
@@ -1,17 +1,17 @@
 from queue import PriorityQueue
 
 class Solution:
-  def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-    que = PriorityQueue()
-    
-    for val in happiness:
-        que.put(-1 * val)
-    
-    turns, result = 0, 0
-    for i in range(k):
-        val = max(0, -1 * que.get() - turns)
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        que = PriorityQueue()
         
-        result += val
-        turns += 1
-    
-    return result
+        for val in happiness:
+            que.put(-1 * val)
+        
+        turns, result = 0, 0
+        for i in range(k):
+            val = max(0, -1 * que.get() - turns)
+            
+            result += val
+            turns += 1
+        
+        return result
