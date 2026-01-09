a04e0ddc671e88185394cc63b89ff1b4a8b47f40
tom-chulhyun <tom.chulhyun@gmail.com>
2026-01-10 00:19:46 +0900

    Update 철현_MaximumMatrixSum.py

--- 1주차/철현_MaximumMatrixSum.py
+++ 1주차/철현_MaximumMatrixSum.py
@@ -1,16 +1,16 @@
 class Solution:
-  def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-    negativeCount, sumMatrix, minValue = 0, 0, 10**5
-    
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if matrix[i][j] < 0:
-                negativeCount += 1
-            
-            minValue = min(minValue, abs(matrix[i][j]))
-            sumMatrix += abs(matrix[i][j])
-    
-    if negativeCount % 2 != 0:
-        sumMatrix -= 2 * minValue
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        negativeCount, sumMatrix, minValue = 0, 0, 10**5
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] < 0:
+                    negativeCount += 1
+                
+                minValue = min(minValue, abs(matrix[i][j]))
+                sumMatrix += abs(matrix[i][j])
+        
+        if negativeCount % 2 != 0:
+            sumMatrix -= 2 * minValue
 
-    return sumMatrix
+        return sumMatrix
